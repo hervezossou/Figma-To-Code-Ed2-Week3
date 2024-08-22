163d194c4216fad12058a89f712f762d70d4b0fb
Hervé Zossou <hervezossou8@gmail.com>
2024-08-22 13:52:28 +0100

    Sidebar styling, responsiveness and dynamically appearance

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+const toggleSidebar = document.getElementById('toggle-sidebar')
+const sidebar = document.getElementById('sidebar')
+
+function showSidebar() {
+    sidebar.classList.toggle('active')
+}
+
+// Use toggleSidebar to show our sidebar dynamically
+toggleSidebar.addEventListener('click', showSidebar)
+
+// Hidding the sidebar nav when user click outside the menu
+
+document.onclick = function(e) {
+    if(sidebar.classList.contains('active') && 
+    !toggleSidebar.contains(e.target) &&
+    !sidebar.contains(e.target)) {
+      sidebar.classList.remove('active')
+    }
+  }
